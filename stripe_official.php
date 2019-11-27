8abe4def542683232e21bd470de7040cd21fff0a
Julien <jhoarau@202-ecommerce.com>
2019-11-27 15:49:28 +0100

    fix bug two Stripe forms with module ps_legalcompliance on Prestashop 1.7

--- stripe_official.php
+++ stripe_official.php
@@ -1253,17 +1253,21 @@ class Stripe_official extends PaymentModule
             return array();
         }
 
-        $payment = $this->hookPayment($params);
+        if (version_compare(_PS_VERSION_, '1.7', '>=')) {
+            $payment = $this->hookPaymentOptions($params);
+        } else {
+            $payment = $this->hookPayment($params);
 
-        Media::addJsDef(array(
-            'stripe_compliance' => true
-        ));
+            Media::addJsDef(array(
+                'stripe_compliance' => true
+            ));
 
-        $payment_options = array(
-            'cta_text' => $this->l('Pay by card'),
-            'logo' => Media::getMediaPath(_PS_MODULE_DIR_.$this->name.'/logo.png'),
-            'form' => $payment
-        );
+            $payment_options = array(
+                'cta_text' => $this->l('Pay by card'),
+                'logo' => Media::getMediaPath(_PS_MODULE_DIR_.$this->name.'/logo.png'),
+                'form' => $payment
+            );
+        }
 
         return $payment_options;
     }
