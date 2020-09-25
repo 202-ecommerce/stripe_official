47ab3f5f3443c928ec68dc654ec9dcd3b15343ce
Lorenz Meyer <lm@bible.ch>
2020-09-25 17:45:30 +0200

    Only display card logos of current shop.

--- stripe_official.php
+++ stripe_official.php
@@ -1074,6 +1074,7 @@ class Stripe_official extends PaymentModule
         $query->from('configuration');
         $query->where('name LIKE "STRIPE_PAYMENT%"');
         $query->where('value = "on"');
+        $query->where('id_shop = '.$this->context->shop->id);
         $results = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($query->build());
 
         foreach ($results as &$result) {
