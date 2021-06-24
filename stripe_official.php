cf7c788ceac1cc5f8f42e188e45df6c60d355b0f
Julien <jhoarau@202-ecommerce.com>
2021-06-24 10:32:06 +0200

    fix bug no credit card logo displayed in front

--- stripe_official.php
+++ stripe_official.php
@@ -1260,7 +1260,9 @@ class Stripe_official extends PaymentModule
         $query->from('configuration');
         $query->where('name LIKE "STRIPE_PAYMENT%"');
         $query->where('value = "on"');
-        $query->where('id_shop = '.$this->context->shop->id);
+        if (Configuration::get('PS_MULTISHOP_FEATURE_ACTIVE') === 1) {
+            $query->where('id_shop = '.$this->context->shop->id);
+        }
         $results = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($query->build());
 
         foreach ($results as &$result) {
