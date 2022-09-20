3f6425eccd7bd96ae0443166c7054c752b3ce425
Afayadas <87704690+Afayadas@users.noreply.github.com>
2022-09-20 11:46:17 +0200

    Fix saved card display in payment option page (#177)

--- classes/StripeCustomer.php
+++ classes/StripeCustomer.php
@@ -91,8 +91,8 @@ class StripeCustomer extends ObjectModel
         $query = new DbQuery();
         $query->select('*');
         $query->from(static::$definition['table']);
-        $query->where('id_customer = ' . pSQL((int) $id_customer));
-        $query->where('id_account = "' . pSQL((int) $id_account) . '"');
+        $query->where('id_customer = ' . (int) $id_customer);
+        $query->where('id_account = "' . pSQL($id_account) . '"');
 
         $result = Db::getInstance(_PS_USE_SQL_SLAVE_)->getRow($query->build());
         if (empty($result) === true) {
